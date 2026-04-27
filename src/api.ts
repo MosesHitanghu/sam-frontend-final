544334239897da59b5083fa714fc0d7455ae3473
Moses Hitanghu <35667052+MosesHitanghu@users.noreply.github.com>
2026-04-27 16:42:05 +0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:9000",
+  baseURL: "https://sam-api-psi.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
